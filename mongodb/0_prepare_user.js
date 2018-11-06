566c574ad2071aaba85e18a36b1528f92e36c851
Sony AK <sony@sony-ak.com>
2018-11-07 01:06:13 +0700

    delete unused script

--- mongodb/0_prepare_user.js
+++
@@ -1,5 +0,0 @@
-db.auth('root', 'mongodbpassword')
-
-db.getSiblingDB('admin')
-
-db.createUser({user:'root', pwd:'mongodbpassword', roles:[{role:'userAdmin', db:'go_db'}, {role:'readWrite', db:'go_db'}]})
