2d0fbb815fb705330055b1af6f837cf43fecef31
Sony AK <sony@lifull-media.id>
2018-11-15 14:52:52 +0700

    add sample: default net/http mux sample

---
+++ src/examples/routingWithDefaultMux.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "io"
+    "log"
+    "net/http"
+)
+
+func serviceDefault(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/" {
+        http.NotFound(w, r)
+        return
+    }
+
+    io.WriteString(w, "this is the / path")
+}
+
+func service1(w http.ResponseWriter, r *http.Request) {
+    io.WriteString(w, "this is the /service1 path")
+}
+
+func service2(w http.ResponseWriter, r *http.Request) {
+    io.WriteString(w, "this is the /service2 path")
+}
+
+func main() {
+    http.HandleFunc("/", serviceDefault)
+    http.HandleFunc("/service1", service1)
+    http.HandleFunc("/service2", service2)
+    
+    log.Fatal(http.ListenAndServe(":3000", nil))
+}
